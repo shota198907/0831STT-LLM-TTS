8183de645e1234837045571591affe57b3c49216
shota198907 <s.nagashima@gift-co.jp>
2025-09-11 07:44:10 +0900

    chore(live): purge legacy tracked files

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import dynamic from "next/dynamic"
-
-const AppRoot = dynamic(() => import("@/components/AppRoot"), { ssr: false })
-
-export default function Page() {
-  return <AppRoot />
-}
